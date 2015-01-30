ed84bd33e3fa3b336eb4f3361ab3976f771e05fc
Zachary Siegel <zsiegel87@gmail.com>
2015-01-29 22:43:11 -0600

    making map function return a list of sections

--- src/main/java/com/zsiegel/java/sandbox/Sandbox.java
+++ src/main/java/com/zsiegel/java/sandbox/Sandbox.java
@@ -14,28 +14,35 @@ import java.util.Map;
 public class Sandbox {
 
     public static void main(String[] args) {
+        List<Section> animalsBySection = mapAnimalsByName();
+        System.out.println(animalsBySection);
+    }
 
-        Observable.from(Animals.list()).toMultimap(new Func1<Animal, String>() {
-            @Override
-            public String call(Animal animal) {
-                return animal.name.substring(0, 1);
-            }
-        }).flatMap(new Func1<Map<String, Collection<Animal>>, Observable<List<Section>>>() {
-            @Override
-            public Observable<List<Section>> call(Map<String, Collection<Animal>> stringCollectionMap) {
+    /**
+     * Take a list of animals and output a list of
+     * sections containing the animals keyed by name
+     */
+    private static List<Section> mapAnimalsByName() {
+        return Observable.from(Animals.list())
+                .toMultimap(new Func1<Animal, String>() {
+                    @Override
+                    public String call(Animal animal) {
+                        return animal.name.substring(0, 1);
+                    }
+                }).flatMap(new Func1<Map<String, Collection<Animal>>, Observable<List<Section>>>() {
+                    @Override
+                    public Observable<List<Section>> call(Map<String, Collection<Animal>> stringCollectionMap) {
 
-                List<Section> sections = new ArrayList<>();
-                for (String sectionTitle : stringCollectionMap.keySet()) {
-                    Section section = new Section();
-                    section.title = sectionTitle;
-                    section.items = stringCollectionMap.get(sectionTitle);
-                    System.out.println(section);
-                    sections.add(section);
-                }
+                        List<Section> sections = new ArrayList<>();
+                        for (String sectionTitle : stringCollectionMap.keySet()) {
+                            Section section = new Section();
+                            section.title = sectionTitle;
+                            section.items = stringCollectionMap.get(sectionTitle);
+                            sections.add(section);
+                        }
 
-                return Observable.just(sections);
-            }
-        }).subscribe();
-
+                        return Observable.just(sections);
+                    }
+                }).toBlocking().first();
     }
 }
--- src/main/kotlin/com/zsiegel/kotlin/sandbox/Sandbox.kt
+++ src/main/kotlin/com/zsiegel/kotlin/sandbox/Sandbox.kt
@@ -4,25 +4,47 @@ import rx.Observable
 import java.util.ArrayList
 import com.zsiegel.java.sandbox.Animals
 import com.zsiegel.java.sandbox.Section
+import com.zsiegel.java.sandbox.Animal
 
 /**
  * @author zsiegel
  */
 fun main(args: Array<String>) {
+    
+    //Standard function call
+    var animalsBySection = mapAnimalsByName();
+    
+    //Extension function tacked on to any instance of List<Animal>
+    //var animalsBySection = Animals.list().mapByName();
+    
+    println(animalsBySection)
+}
 
-    Observable.from(Animals.list())
+/**
+ * An extension method available on any instance of List<Animal>
+ */
+fun List<Animal>.mapByName() : List<Section> {
+    return mapAnimalsByName();
+}
+
+/**
+ * Take a list of animals and output a list of
+ * sections containing the animals keyed by name
+ */
+fun mapAnimalsByName() : List<Section> {
+    return Observable.from(Animals.list())
             .toMultimap { animal ->
                 animal.name.substring(0, 1);
             }.flatMap { mapOfAnimals ->
+        
                 val sections = ArrayList<Section>();
-                    for (sectionTitle in mapOfAnimals.keySet()) {
+                for (sectionTitle in mapOfAnimals.keySet()) {
                     val section = Section()
                     section.title = sectionTitle
                     section.items = mapOfAnimals.get(sectionTitle)
-                    println(section)
                     sections.add(section)
                 }
-                Observable.just(sections)
-            }.subscribe();
 
+                Observable.just(sections)
+            }.toBlocking().first();
 }
